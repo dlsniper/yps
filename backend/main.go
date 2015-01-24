d03a05766c5ccc0ea23b037da27adbe8adabc110
Florin Patan <florinpatan@gmail.com>
2015-01-24 20:04:02 +0100

    First working cron + task queue

--- backend/main.go
+++ backend/main.go
@@ -2,13 +2,35 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+
+	"appengine"
+	"appengine/taskqueue"
+
+	"github.com/gorilla/mux"
 )
 
 func init() {
-	http.HandleFunc("/", handler)
+	r := mux.NewRouter()
+	r.HandleFunc("/processUserInput", queueHandler).Methods("GET")
+	http.Handle("/", r)
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello, backend world!")
+func queueHandler(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+
+	tasks, err := taskqueue.Lease(c, 10, "userInput", 60)
+
+	if err != nil {
+		log.Printf("[error] Task failed: %#v", err)
+		return
+	}
+
+	for _, task := range tasks {
+		log.Printf("Got task: %#v", task)
+		taskqueue.Delete(c, task, "userInput")
+	}
+
+	fmt.Fprint(w, "it worked")
 }
